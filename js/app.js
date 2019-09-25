f76bbc4a3a130a5339214caf2c764ab86ce430fa
sviveros05@gmail.com <sviveros05@gmail.com>
2019-09-25 12:08:20 -0500

    Created a playerShip

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// console.log('working');
+
+
+
+// make a function that will have player attack the alien every time invoked
+// create player object and alien object
+
+
+// const playerShip = {
+// 	name: 'USS Assembly'
+// 	hull:
+// 	firepower:
+// 	accuracy;
+// }
+
+
+
+// const alienShip = {
+
+// }
